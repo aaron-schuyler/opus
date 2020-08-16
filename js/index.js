3ba0c737df40058f9a839d9c6f73a450a5e90bb5
aaron-schuyler <asky28@gmail.com>
2020-08-15 21:49:54 -0400

    building editor functions

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const main = document.querySelector('#main')
 const editor = document.createElement('div')
 const editorWrapper = document.createElement('div')
+const files = document.createElement('div')
+
+files.classList.add('files')
 
 editor.id = 'editor'
 editorWrapper.append(editor)
@@ -10,7 +13,20 @@ const quill = new Quill('#editor', {
   theme: 'snow'
 })
 
-function showEditor(content = editor.innerHTML) {
-  editor.innerHTML = content
+function showEditor(content = quill.getContents()) {
+  files.remove()
+  quill.setContents(content)
   main.append(editorWrapper)
+}
+
+function newFile() {
+  files.remove()
+  editorWrapper.remove()
+  showEditor({})
+}
+
+function showFileNavigator() {
+  editorWrapper.remove()
+  main.append(files)
+  files.innerText = 'hello'
 }
