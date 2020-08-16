a056a58ad038ec55215754297d420b58f7441910
aaron-schuyler <asky28@gmail.com>
2020-08-15 21:15:33 -0400

    set up basic quill setup

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 const main = document.querySelector('#main')
+const editor = document.createElement('div')
+const editorWrapper = document.createElement('div')
+
+editor.id = 'editor'
+editorWrapper.append(editor)
+main.append(editorWrapper)
+
+const quill = new Quill('#editor', {
+  theme: 'snow'
+})
+
+function showEditor(content = editor.innerHTML) {
+  editor.innerHTML = content
+  main.append(editorWrapper)
+}
