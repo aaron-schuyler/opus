f058b8b2111aceac2fd21980145afce5c9582f9c
aaron-schuyler <asky28@gmail.com>
2020-08-16 15:50:15 -0400

    layout css work on files page

--- js/index.js
+++ js/index.js
@@ -3,6 +3,34 @@ const editor = document.createElement('div')
 const editorWrapper = document.createElement('div')
 const files = document.createElement('div')
 
+const fakeFiles = [
+  {
+    title: 'Lorum Ipsum',
+    body: 'Lorum Ipsum Sit Dolor, Lorum Ipsum Sit Dolor: A Lorum Ipsum Story, Lorum Ipsum Sit Dolor, Lorum Ipsum Sit Dolor: A Lorum Ipsum Story.',
+    collection: 1
+  },
+  {
+    title: 'Lorum Ipsum 2',
+    body: 'Lorum Ipsum Sit Dolor, A Lorum Ipsum Story...',
+    collection: 1
+  },
+  {
+    title: 'Lorum Ipsum 3',
+    body: 'Lorum Ipsum Sit Dolor, A Lorum Ipsum Story...',
+    collection: 1
+  },
+  {
+    title: 'Lorum Ipsum 4',
+    body: 'Lorum Ipsum Sit Dolor, A Lorum Ipsum Story...',
+    collection: 1
+  },
+  {
+    title: 'Lorum Ipsum 5',
+    body: 'Lorum Ipsum Sit Dolor, A Lorum Ipsum Story...',
+    collection: 1
+  }
+]
+
 files.classList.add('files')
 
 editor.id = 'editor'
@@ -20,7 +48,7 @@ function showEditor(content = quill.getContents()) {
 }
 
 function newFile() {
-  files.remove()
+  save() // use .then to call show editor
   editorWrapper.remove()
   showEditor({})
 }
@@ -28,5 +56,24 @@ function newFile() {
 function showFileNavigator() {
   editorWrapper.remove()
   main.append(files)
-  files.innerText = 'hello'
+  for (file of fakeFiles) {
+    let fileCard = document.createElement('div')
+    let title = document.createElement('h3')
+    let body = document.createElement('p')
+    let fade = document.createElement('span')
+    fileCard.append(title, body, fade)
+
+    fileCard.classList.add('file')
+
+    title.innerText = file.title
+    body.innerText = file.body
+
+    files.append(fileCard)
+  }
 }
+
+function save() {
+
+}
+
+//showFileNavigator()
