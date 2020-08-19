2c6f981345d8e132d3e81212b9890f332ed23ef9
aaron-schuyler <asky28@gmail.com>
2020-08-19 13:28:07 -0400

    fresh start on the front end

--- js/index.js
+++ js/index.js
@@ -1,79 +1,18 @@
-const main = document.querySelector('#main')
-const editor = document.createElement('div')
-const editorWrapper = document.createElement('div')
-const files = document.createElement('div')
-
-const fakeFiles = [
-  {
-    title: 'Lorum Ipsum',
-    body: 'Lorum Ipsum Sit Dolor, Lorum Ipsum Sit Dolor: A Lorum Ipsum Story, Lorum Ipsum Sit Dolor, Lorum Ipsum Sit Dolor: A Lorum Ipsum Story.',
-    collection: 1
-  },
-  {
-    title: 'Lorum Ipsum 2',
-    body: 'Lorum Ipsum Sit Dolor, A Lorum Ipsum Story...',
-    collection: 1
-  },
-  {
-    title: 'Lorum Ipsum 3',
-    body: 'Lorum Ipsum Sit Dolor, A Lorum Ipsum Story...',
-    collection: 1
-  },
-  {
-    title: 'Lorum Ipsum 4',
-    body: 'Lorum Ipsum Sit Dolor, A Lorum Ipsum Story...',
-    collection: 1
-  },
-  {
-    title: 'Lorum Ipsum 5',
-    body: 'Lorum Ipsum Sit Dolor, A Lorum Ipsum Story...',
-    collection: 1
-  }
-]
-
-files.classList.add('files')
-
-editor.id = 'editor'
-editorWrapper.append(editor)
-main.append(editorWrapper)
-
-const quill = new Quill('#editor', {
-  theme: 'snow'
-})
-
-function showEditor(content = quill.getContents()) {
-  files.remove()
-  quill.setContents(content)
-  main.append(editorWrapper)
-}
-
-function newFile() {
-  save() // use .then to call show editor
-  editorWrapper.remove()
-  showEditor({})
-}
-
-function showFileNavigator() {
-  editorWrapper.remove()
-  main.append(files)
-  for (file of fakeFiles) {
-    let fileCard = document.createElement('div')
-    let title = document.createElement('h3')
-    let body = document.createElement('p')
-    let fade = document.createElement('span')
-    fileCard.append(title, body, fade)
-
-    fileCard.classList.add('file')
-
-    title.innerText = file.title
-    body.innerText = file.body
-
-    files.append(fileCard)
-  }
-}
-
-function save() {
-
-}
-
-//showFileNavigator()
+// //on page load we should check the user's credentials
+//   if user_logged_in
+//     remove_login_form
+//     show_folders
+//
+// // handle login form
+//   SessionAdapter
+//
+// // handle logout
+//   SessionAdapter
+//
+// //handle navigation
+//   new_doc_button
+//   doc_navigator_button
+//
+// //handle tabs
+//   show_doc when tab_clicked
+//   remove_tab when x_clicked
