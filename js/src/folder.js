7a60c5a823dd638c7eb8b1aa258e45a5aa6f544c
aaron-schuyler <asky28@gmail.com>
2020-08-19 20:25:13 -0400

    folder view complete with basic styling

--- js/src/folder.js
+++ js/src/folder.js
@@ -16,16 +16,15 @@ class Folder {
   get folderIcon() {
     const div = document.createElement('div')
     div.id = 'folder-' + this.id
-    div.classList.add('folder-icon', 'folder-' + this.color)
-
+    div.classList.add('folder-icon', 'folder-color-' + this.color)
+    div.title = this.name
     const icon = document.createElement('i')
     icon.classList.add('fa', 'fa-folder')
-
     const name = document.createElement('span')
     name.innerText = this.name
     name.classList.add('folder-name')
-
     div.append(icon, name)
+    div.addEventListener('click', this.goToFolder)
     return div
   }
   static showAllFolders() {
@@ -34,7 +33,7 @@ class Folder {
 
   static generateCollectionView(id) {
     const div = document.createElement('div')
-    div.classList.add('colldection')
+    div.classList.add('collection')
     div.id = id
     return div
   }
